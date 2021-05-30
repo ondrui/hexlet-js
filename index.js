efa4ec5e178397ece1e6a5e25685c860682d5d3f
ondrui <ondrui@yandex.ru>
2021-05-30 18:14:52 +0300

    add lodash

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import _ from "lodash";
+
 console.log("Hello, Hexlet!");
+// Пересечение массивов
+console.log(_.intersection([2, 1], [2, 3])); // => [2]
+
+// capitalize делает первую букву заглавной
+console.log(_.capitalize("hello")); // Hello
+
+console.log(_.last(['one', 'two'])); //two
+
+
+
